f77173a80d756999733da557d4f70b066871e3c7
MiriColin <73191998+MiriColin@users.noreply.github.com>
2020-10-21 13:44:06 -0500

    Update genera_figuras.R

--- Hidden_files/Tesis/Programas/Figuras PDF/genera_figuras.R
+++ Hidden_files/Tesis/Programas/Figuras PDF/genera_figuras.R
@@ -9,7 +9,7 @@
 rm(list=ls())  # Borra variables
 cat("\014") # Borra consola
 #Se establece el directorio en el que se va a trabajar
-setwd("C:/Users/miri_/Dropbox/Carpeta compartida MIri/Faculty_schedule_simulation\Hidden_files/Tesis")
+setwd("C:/Users/miri_/Dropbox/Carpeta compartida MIri/Faculty_schedule_simulation\Hidden_files/Tesis/Programas/Figuras PDF")
 
 # install.packages('randomcoloR')
 # install.packages('fitdistrplus')
@@ -33,66 +33,66 @@ param_graficas$dir_TeX = "TeX/LaTeX/Pictures/"
 # Figuras -----------------------------------------------------------------
 
 ##### Número de alumnos totales por semestres pares e impares
-source("Programas/Figuras PDF/fig_num_alum_sem_par_impar_Proba_I/num_alum_sem_par_impar_Proba_I.R")
+source("fig_num_alum_sem_par_impar_Proba_I/num_alum_sem_par_impar_Proba_I.R")
 
 ##### Histograma del número de alumnos por semestre
-source("Programas/Figuras PDF/fig_num_alum_x_turno_Proba_I/num_alum_x_turno_Proba_I.R")
+source("fig_num_alum_x_turno_Proba_I/num_alum_x_turno_Proba_I.R")
 
 ##### Número de alumnos por turno
-source("Programas/Figuras PDF/fig_histograma_FR_num_alum_x_turno_Proba_I/histograma_FR_num_alum_x_turno_Proba_I.R")
+source("fig_histograma_FR_num_alum_x_turno_Proba_I/histograma_FR_num_alum_x_turno_Proba_I.R")
 
 ##### Histograma del número de alumnos por turno
-source("Programas/Figuras PDF/fig_histograma_FR_num_alum_x_turno_Proba_I/histograma_FR_num_alum_x_turno_Proba_I.R")
+source("fig_histograma_FR_num_alum_x_turno_Proba_I/histograma_FR_num_alum_x_turno_Proba_I.R")
 
 ##### Número total de alumnos por semestre
-source("Programas/Figuras PDF/fig_num_alum_total_x_sem_barplot/num_alum_total_x_sem_barplot.R")
+source("fig_num_alum_total_x_sem_barplot/num_alum_total_x_sem_barplot.R")
 
 ##### Media de alumnos por semestre
-source("Programas/Figuras PDF/fig_prom_alum_total_x_sem_ts/prom_alum_total_x_sem_ts.R")
+source("fig_prom_alum_total_x_sem_ts/prom_alum_total_x_sem_ts.R")
 
 ##### Desviación estándar del número de alumnos por semestre
-source("Programas/Figuras PDF/fig_sd_alum_x_gpo_x_sem_ts/sd_alum_x_gpo_x_sem_ts.R")
+source("fig_sd_alum_x_gpo_x_sem_ts/sd_alum_x_gpo_x_sem_ts.R")
 
 ##### Histograma del número de alumnos de todos los semestres
-source("Programas/Figuras PDF/fig_histograma_num_alum_x_gpo_x_sem/histograma_num_alum_x_gpo_x_sem.R")
+source("fig_histograma_num_alum_x_gpo_x_sem/histograma_num_alum_x_gpo_x_sem.R")
 # graphics.off()#Código para limpiar las gráficas porque este histograma usa la función par()
 
 ##### Histograma del número de alumnos de semestres pares e impares
-source("Programas/Figuras PDF/fig_histograma_FA_num_alum_sem_par_impar/histograma_FA_num_alum_sem_par_impar.R")
+source("fig_histograma_FA_num_alum_sem_par_impar/histograma_FA_num_alum_sem_par_impar.R")
 
 ##### Histograma del número de alumnos de los turnos matutino y vespertino
-source("Programas/Figuras PDF/fig_histograma_FA_num_alum_matu_vesp/histograma_FA_num_alum_matu_vesp.R")
+source("fig_histograma_FA_num_alum_matu_vesp/histograma_FA_num_alum_matu_vesp.R")
 
 ##### Descomposición por el método aditivo de Holt-Winters: Total de alumnos por semestre
-source("Programas/Figuras PDF/fig_descomposicion_ts_total_alumnos/descomposicion_ts_total_alumnos.R")
+source("fig_descomposicion_ts_total_alumnos/descomposicion_ts_total_alumnos.R")
 
 ##### Número promedio de grupos por hora
-source("Programas/Figuras PDF/fig_num_prom_gpos_x_hora_barplot/num_prom_gpos_x_hora_barplot.R")
+source("fig_num_prom_gpos_x_hora_barplot/num_prom_gpos_x_hora_barplot.R")
 
 ##### Número promedio de alumnos por hora
-source("Programas/Figuras PDF/fig_prom_alum_x_hora_barplot/prom_alum_x_hora_barplot.R")
+source("fig_prom_alum_x_hora_barplot/prom_alum_x_hora_barplot.R")
 
 ##### Número de alumnos por grupo de todos los semestres
-source("Programas/Figuras PDF/fig_num_alum_sem_par_impar_ts/num_alum_sem_par_impar_ts.R")
+source("fig_num_alum_sem_par_impar_ts/num_alum_sem_par_impar_ts.R")
 
 ##### Número de alumnos por turno de todos los semestres
-source("Programas/Figuras PDF/fig_num_alum_matu_vesp_ts/num_alum_matu_vesp_ts.R")
+source("fig_num_alum_matu_vesp_ts/num_alum_matu_vesp_ts.R")
 
 ##### Densidades del número de alumnos por grupo de cada semestre
-source("Programas/Figuras PDF/fig_densidades_num_alum_x_gpo_x_sem/densidades_num_alum_x_gpo_x_sem.R")
+source("fig_densidades_num_alum_x_gpo_x_sem/densidades_num_alum_x_gpo_x_sem.R")
 
 ##### Densidad ajustada del tamaño de grupo
-source("Programas/Figuras PDF/fig_densidad_num_alum_x_gpo_x_sem/densidad_num_alum_x_gpo_x_sem.R")
+source("fig_densidad_num_alum_x_gpo_x_sem/densidad_num_alum_x_gpo_x_sem.R")
 
 ##### Histograma del número de alumnos de semestres pares e impares
-source("Programas/Figuras PDF/fig_histograma_FR_num_alum_sem_par_impar/histograma_FR_num_alum_sem_par_impar.R")
+source("fig_histograma_FR_num_alum_sem_par_impar/histograma_FR_num_alum_sem_par_impar.R")
 
 ##### Histograma del número de alumnos de los turnos matutino y vespertino
-source("Programas/Figuras PDF/fig_histograma_FR_num_alum_matu_vesp/histograma_FR_num_alum_matu_vesp.R")
+source("fig_histograma_FR_num_alum_matu_vesp/histograma_FR_num_alum_matu_vesp.R")
 
 #####
-# source("Programas/Figuras PDF//.R")
+# source("/.R")
 
 #####
-# source("Programas/Figuras PDF//.R")
+# source("/.R")
 
