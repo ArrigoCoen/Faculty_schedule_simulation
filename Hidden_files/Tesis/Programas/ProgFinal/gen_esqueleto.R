914fe869c7f29cf8ff6aa83e4f9bff3ad0d3a2f5
MiriColin <73191998+MiriColin@users.noreply.github.com>
2020-10-23 11:23:49 -0500

    Create gen_esqueleto.R

---
+++ Hidden_files/Tesis/Programas/ProgFinal/gen_esqueleto.R
@@ -0,0 +1,23 @@
+##########################################################################
+#' En este programa se encuentra la función que genera la asignación
+#' completa de todas las materias.
+##########################################################################
+
+#Source ------------------------------------------------------------------
+#Se utiliza el comando source() para poder cargar todas las funciones que
+#se encuentran en el archivo Fn_Asignación.
+rm(list=ls())  # Borra variables
+cat("\014") # Borra consola
+#Se establece el directorio en el que se va a trabajar
+setwd("C:/Users/miri_/Dropbox/Carpeta compartida MIri/Faculty_schedule_simulation/Hidden_files/Tesis/Programas/ProgFinal")
+source("Fn_Asignacion.R")
+
+
+# gen_esqueleto -----------------------------------------------------------
+gen_esqueleto <- function(){
+  #Se definen las variables que se van a utilizar
+  
+  
+  
+  return(mat_esqueleto)
+}
